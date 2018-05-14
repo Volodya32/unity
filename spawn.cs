7311312b757087dec73d8544c676ed96c63b5a15
Volodya32 <34636058+Volodya32@users.noreply.github.com>
2018-05-14 10:29:15 +0900

    Add files via upload

---
+++ spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class spawn : MonoBehaviour {
+	public GameObject zombie;
+	Transform spawnz;
+	//NavMeshAgent nav;
+	public Transform MainCamera;
+	GameObject clone;
+	// Use this for initialization
+	void Start () {		
+		InvokeRepeating ("spawnerZombie", 2f, 10f);
+		spawnz = GetComponent<Transform> ();
+
+	}
+	void spawnerZombie(){
+		clone = Instantiate(zombie,spawnz.position,spawnz.rotation);
+		clone.GetComponent<NavMeshAgent>().SetDestination(MainCamera.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
